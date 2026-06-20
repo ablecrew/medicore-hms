932e460a7e382ac92905f71c0c534649aaa44445
ablecrew <teddy.dande4@gmail.com>
2026-06-20 12:26:34 +0300

    mobile sidebar nav

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -24,7 +24,7 @@ export default function AppLayout() {
 
       {/* MOBILE BOTTOM NAV */}
       <div className="fixed inset-x-0 bottom-0 z-50 md:hidden">
-        <BottomNav />
+        <Sidebar />
       </div>
     </div>
   );
